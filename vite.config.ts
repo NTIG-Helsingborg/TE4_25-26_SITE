90cdb95bdd8ac2e261442ccca22f77a274fade04
Jesper <lillebrorjesper@gmail.com>
2026-05-27 09:11:36 +0200

    site not loading right fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/TE4_25-26_SITE/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
